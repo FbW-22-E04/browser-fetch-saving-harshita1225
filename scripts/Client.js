495c14de855bfb13022629575e3d0f88830ef081
harshita <harshita1225@gmail.com>
2022-10-05 22:50:24 +0200

    task completed

--- scripts/Client.js
+++ scripts/Client.js
@@ -1,14 +1,16 @@
 class Client {
-    constructor () {
-        //Your token goes here
-        this.token = "";
-    }
+  constructor() {
+    //Your token goes here
+    this.token = "9f010e4f";
+  }
 
-    async getMovieData(movie) {
-        let response = await fetch(`http://www.omdbapi.com/?t=${movie}&apikey=${this.token}`);
-        let data = await response.json();
-        return data;
-    }
+  async getMovieData(movie) {
+    let response = await fetch(
+      `http://www.omdbapi.com/?t=${movie}&apikey=${this.token}`
+    );
+    let data = await response.json();
+    return data;
+  }
 }
 
 console.log("Hello from Client.js");
--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,47 @@
-import View from './View.js';
-import Client from './Client.js';
+import View from "./View.js";
+import Client from "./Client.js";
 
 // All of your javascript should go here
-console.log("Hello from index.js");
+
+const newView = new View();
+const newClient = new Client();
+
+const save = document.querySelector(".btn-save");
+const reset = document.querySelector(".btn-reset");
+const input = document.getElementById("input");
+const movieList = [];
+input.addEventListener("change", async () => {
+  if (input.value) {
+    const response = await newClient.getMovieData(input.value);
+
+    let data = newView.displayMovieOnPage(response);
+    console.log(response);
+
+    movieList.push(response);
+
+    input.value = "";
+  }
+});
+
+//store values as local storage
+save.addEventListener("click", () => {
+  localStorage.setItem("movie", JSON.stringify(movieList));
+});
+
+// movieDisplay using local storage
+function movieDisplay() {
+  const dataFromLocal = JSON.parse(localStorage.getItem("movie"));
+
+  movieList.push(...dataFromLocal);
+  movieList.forEach((item) => {
+    newView.displayMovieOnPage(item);
+  });
+}
+movieDisplay();
+
+reset.addEventListener("click", function () {
+  window.location.reload();
+  newView.removeDisplay();
+  localStorage.removeItem("movie");
+  movieList.splice(0);
+});
